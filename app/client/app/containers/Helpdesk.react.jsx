c5826f59fa56553456adf303f20757215dc5861b
Daan <daan@zimmermanzimmerman.nl>
2016-06-20 14:20:02 +0200

    text changes

--- app/client/app/containers/Helpdesk.react.jsx
+++ app/client/app/containers/Helpdesk.react.jsx
@@ -15,40 +15,44 @@ import { toggleMainMenu } from '../actions/sync'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 const faq = [{
-	question: 'What is this app all about?',
-	answer: '<p>IATI Studio gives you access to the entire <a href="http://www.iatiregistry.org/" target="_blank">IATI Registry</a> and enables you to visualise its data. </p>'
+	question: 'What is IATI Studio?',
+	answer: '<p>IATI Studio provides you with direct access to the all the data published to the <a href="http://www.iatiregistry.org/" target="_blank">IATI Registry</a> and enables you to visualise and analyse those data.</p>'
 	},
 	{
-	question: 'Can I access everything in the IATI Registry?',
-	answer: '<p>Yes you can. We pull the data straight from the registry using our <a href="https://www.oipa.nl/" target="_blank">OIPA data engine</a> every night, so the data in IATI Studio is always up to date.</p>'
+	question: 'Can I access all data published in the IATI Registry?',
+	answer: '<p>Yes. IATI Studio pulls data directl;y from the registry using the <a href="https://www.oipa.nl/" target="_blank">OIPA data engine</a> every night, so the data in IATI Studio is always up to date.</p>'
 	},
 	{
 	question: 'Can I publish my own IATI data using IATI Studio?',
-	answer: '<p>Not yet, but we\'re working hard to make this possible soon, using the same easy to use interface as our chart builder.</p>'
+	answer: '<p>Not yet, but this will be operational this year. This Data Manager will be making use of the same easy to use interface as our chart builder.</p>'
 	},
 	{
-	question: 'When will your micro site builder be ready?',
-	answer: '<p>We\'re aiming for a release in the autumn of 2016.</p>'
+		question:'I can not find my organisation',
+		answer:'If your organisation is not listed, your IATI data is probably not up to date. IATI Studio can only show and use validated data. Please check the list or organisations that we can not process in IATI Studio and see if your organisation is on that list. Once you have fixed the issue - usually invalid transaction dates- IATI will pick up your data. If you are not on that list and not listed in IATI Studio either, please contact us for information at: <a href="mailto:enquiry@iatistudio.com">enquiry@iatistudio.com</a>.'
+	},
+	{
+		question:'When will your Microsite Builder be ready?',
+		answer:'IATI Studio aims for a release in the autumn of 2016.'
 	},
 	{
 	question: 'Is there a limit to how many charts I can build?',
-	answer: '<p>Yes there is, we\'ve currently set this at 20 charts. The charts residing in your trash also count towards this limit, so if you\'re having trouble, be sure to empty the trash regularly.</p><p>For the paid versions that we\'ll release later this year, this limit will be different of course. See our <a href="https://www.iatistudio.com/membership/" target="_blank">pricing page</a> for details.</p>'
+	answer: '<p>For the Enterprise versions that will be releasde later this year, this limit will be different. See our pricing page for more details.</p><p>For the Enterprise versions that will be released later this year, this limit will be different. See our <a href="https://www.iatistudio.com/membership/" target="_blank">pricing page</a> for more details.</p>'
 	},
 	{
-	question: 'I don\'t get the chart embedding feature',
-	answer: '<p>You can embed charts using the iframe HTML code we provide on your chart\'s public page. You can copy this iframe code into your own website\'s HTML. If you\'re using a CMS like WordPress, be sure to paste the code in the <i>code</i> view of the page editor.</p><p>Unfortunately iframe heights are not really responsive for mobile viewing, so you might see a scrollbar sometimes. You can play around with the <i>width</i> and <i>height</i> settings in the embed code to see what works best for you.</p>'
+	question: 'I don\'t understand the chart embed feature',
+	answer: '<p>You can embed charts using the iframe HTML code that\'s provided on your chart\'s public page. You can copy this iframe code into your personal or company website editor. If you\'re using a CMS like WordPress, make sure to paste the code in the <i>code</i> view of the page editor.</p><p>Unfortunately iframe heights are not really responsive for mobile viewing, so you may see a scrollbar sometimes. You can play around with the <i>width</i> and <i>height</i> settings in the embed code to see what works best for you.</p>'
 	},
 	{
-	question: 'How does the community feed work?',
-	answer: '<p>The community feed consists of all the charts that have been published. As soon as you hit <i>publish</i> in your chart, it will appear in the public feed. For a quick overview of which charts you have published, have a look in your library. Public charts are flagged with a globe icon.</p>'
+	question: 'What is the Community Feed?',
+	answer: '<p>The community feed consists of all the charts that have been published in IATI Studio. As soon as you hit <i>publish</i> in your chart, it will appear in the Community Feed. For a quick overview of which charts you have published, have a look in your library. Public charts are flagged with a green globe icon.</p>'
 	},
 	{
 	question: 'Can I share my unpublished charts with someone else?',
-	answer: '<p>No you can\'t. You have to publish your chart to enable sharing, otherwise only you are the only one with access.</p>'
+	answer: '<p>No you can not. You must publish your chart to enable sharing or duplication.</p>'
 	},
 	{
-	question: 'I\'ve found a bug!',
-	answer: '<p>Oh no! Good thing we\'re still in beta. Please let us know what happened and when it happened, what you were doing, and anything else you might find relevant, on <a href="mailto:enquiry@iatistudio.com">enquiry@iatistudio.com</a> and we\'ll try and fix it as soon as possible.</p>'
+	question: 'Something is not working!',
+	answer: '<p>Oh no! Good thing IATI Studio is still in Beta. Please let us know what happened and when it happened, what you were doing, and anything else you might find relevant, on <a href="mailto:enquiry@iatistudio.com">enquiry@iatistudio.com</a> and we\'ll try and fix it as soon as possible.</p>'
 	},
 ]
 
