9790fee2c1cfd9944f694f52586e6be2797dcb9c
AlessandroMendozza <alessandro.mendozza@gmail.com>
2016-09-08 17:36:27 +0200

    implementation of a dynamic selector

--- app/client/app/components/orgSettings/OrgWrapper.js
+++ app/client/app/components/orgSettings/OrgWrapper.js
@@ -2,14 +2,14 @@
 
 import React from 'react'
 import { PageTitle, Button, PageTitleButtonsGroup1 }       from './OrgComponentsList'
-import SelectReactComponentsList       from './SelectReactComponentsList'
+import MultiSelectField       from '../react-select/MultiSelect'
 
 let OrgWrapper = function(props) {
   return (
     <div id="orgWrapper">
       <div className="rowPub">
         <PageTitleButtonsGroup1 pageTitleContent="Organisation settings" />
-        <Select />
+        <MultiSelectField />
       </div>
     </div>
   )
