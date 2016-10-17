a3089c268fcb0b37e8ceafc410f5d7bd6569e27c
Daan <daan@zimmermanzimmerman.nl>
2016-10-17 14:40:47 +0200

    cleanup of pub.settings form

--- app/client/app/components/publisher/PublisherApiKey.js
+++ app/client/app/components/publisher/PublisherApiKey.js
@@ -11,24 +11,43 @@ import {Tooltip} from '../general/Tooltip.react.jsx'
 let PublisherApiKey = React.createClass({
 
   getInitialState: function () {
-    return {}
+    return {
+      userId: '',
+      apiKey: '',
+    }
   },
 
   componentWillReceiveProps(nextProps){
     if (nextProps.publisher.validationStatus){
-      this.refs.apiKey.value = nextProps.publisher.apiKey
-      this.refs.userId.value = nextProps.publisher.userId
+      this.setState({
+        userId: nextProps.publisher.userId,
+        userIdError: false,
+        apiKey: nextProps.publisher.apiKey,
+        apiKeyError: false,
+      })
     }
   },
 
-  validate: function () {
-    const apiKey = this.refs.apiKey.value
-    const userId = this.refs.userId.value
-    this.props.getApiKeyValidation(apiKey, userId)
+  handleSubmit: function (e) {
+    e.preventDefault()
+    if (this.props.publisher.validationStatus) {
+      this.props.getApiKeyUnlink(this.props.publisher)
+    } 
+    else {
+      this.props.getApiKeyValidation(this.state.apiKey, this.state.userId)
+    }
   },
 
   unvalidate: function (){
     this.props.getApiKeyUnlink(this.props.publisher)
+  },
+
+  handleChangeUserId: function(e){
+    this.setState({userId: e.target.value})
+  },
+
+  handleChangeApiKey: function(e){
+    this.setState({apiKey: e.target.value})
   },
 
   render: function () {
@@ -37,35 +56,29 @@ let PublisherApiKey = React.createClass({
       valid: this.props.publisher.validationStatus,
       invalid: !this.props.publisher.validationStatus
     })
-    let validationValue = this.props.publisher.validationStatus ? "VALIDATED" : "NOT VALIDATED"
-
-    let validationButton;
-    if(this.props.publisher.validationStatus){
-      validationButton = <SubmitButton value="Unlink" onClick={this.unvalidate} />
-    } else {
-      validationButton = <SubmitButton value="Validate" onClick={this.validate} />
-    }
 
     return (
       <div>
-        <div className="row">
-          <div className="columns medium-4">
-            <h6 className="with-tip">IATI Registry user ID</h6>
-            <Tooltip className="inline" tooltip="Use: zimmzimm"><i className="material-icons">info</i></Tooltip>
-            <input ref="userId" placeholder="User ID" type="text" />
+        <form onSubmit={this.handleSubmit} >
+          <div className="row">
+            <div className="columns medium-4">
+              <h6 className="with-tip">IATI Registry user ID</h6>
+              <Tooltip className="inline" tooltip="Use: zimmzimm"><i className="material-icons">info</i></Tooltip>
+              <input placeholder="User ID" type="text" value={this.state.userId} onChange={this.handleChangeUserId}/>
+            </div>
+            <div className="columns medium-8">
+              <h6 className="with-tip">IATI Registry API key</h6>
+              <Tooltip className="inline" tooltip="Use: 42664fcd-2494-4bab-92fe-5af6113d55a6"><i className="material-icons">info</i></Tooltip>
+              <input placeholder="API Key" type="text" value={this.state.apiKey} onChange={this.handleChangeApiKey} />
+            </div>
           </div>
-          <div className="columns medium-8">
-            <h6 className="with-tip">IATI Registry API key</h6>
-            <Tooltip className="inline" tooltip="Use: 42664fcd-2494-4bab-92fe-5af6113d55a6"><i className="material-icons">info</i></Tooltip>
-            <input ref="apiKey" placeholder="API Key" type="text" />
-          </div>
-        </div>
-        {validationButton}
+          <input value={this.props.publisher.validationStatus ? 'Unlink from registry' : 'Validate'} type="submit" className="button"/>
+        </form>
 
         <h6 className="with-tip">Current validation status</h6>
         <Tooltip className="inline" tooltip="Info text goes here"><i className="material-icons">info</i></Tooltip>
         <div className={validationClass}>
-          {validationValue}
+          {this.props.publisher.validationStatus ? "VALIDATED" : "NOT VALIDATED"}
         </div>
 
       </div>
