a555f1e8340d3867a76dc3fdf219142dc1d84de4
Mayur Gaud <mayur_gaud99@yahoo.com>
2016-11-09 15:16:10 +0100

    Corrected the identation

--- app/client/app/components/activity/forms/ClassificationSectorForm.js
+++ app/client/app/components/activity/forms/ClassificationSectorForm.js
@@ -4,124 +4,124 @@ import {Tooltip} from '../../general/Tooltip.react.jsx'
 import {renderNarrativeFields, renderField} from '../helpers/FormHelper'
 
 const renderSectorTypeSelect = ({name, label, meta: {touched, error}}) => (
-    <div className="columns small-6">
-        <div>
-            <label>{label}</label>
-            <div>
-                <Field name={name} component="select">
-                    <option></option>
-                </Field>
-            </div>
-            {touched && error && <span className="error">{error}</span>}
-        </div>
+  <div className="columns small-6">
+    <div>
+      <label>{label}</label>
+      <div>
+        <Field name={name} component="select">
+          <option></option>
+        </Field>
+      </div>
+      {touched && error && <span className="error">{error}</span>}
     </div>
+  </div>
 );
 
 const renderSector = ({fields, languageOptions, meta: {touched, error}}) => (
-    <div>
-        {fields.map((description, index) =>
-            <div className="field-list" key={index}>
-                <div className="row">
-                    <Field component={renderSectorTypeSelect} name={`${description}.region[code]`} label="Sector Vocabulary"/>
-                    <div className="columns small-6">
-                        <Field
-                            name="uriSectorText"
-                            type="text"
-                            component={renderField}
-                            label="Vocabulary URI"
-                        />
-                    </div>
-                    <Field component={renderSectorTypeSelect} name={`${description}.vocabulary[code]`} label="Sector Code"/>
-                    <div className="columns small-6">
-                        <Field
-                            name="SectorText"
-                            type="text"
-                            component={renderField}
-                            label="Percentage"
-                        />
-                    </div>
-                </div>
-                <div className="row">
-                    <FieldArray
-                        name={`${description}.additionalSector`}
-                        component={renderNarrativeFields}
-                        languageOptions={languageOptions}
-                        textName="textSector"
-                        textLabel="Title"
-                    />
-                </div>
-            </div>
-        )}
-        <div className="columns">
-            <button className="control-button add" type="button" onClick={() => fields.push({})}>Add More</button>
-            <button
-                type="button"
-                title="Remove Title"
-                className="control-button remove float-right"
-                onClick={() => fields.pop()}>Delete
-            </button>
-            {touched && error && <span className="error">{error}</span>}
+  <div>
+    {fields.map((description, index) =>
+      <div className="field-list" key={index}>
+        <div className="row">
+          <Field component={renderSectorTypeSelect} name={`${description}.region[code]`} label="Sector Vocabulary"/>
+          <div className="columns small-6">
+            <Field
+              name="uriSectorText"
+              type="text"
+              component={renderField}
+              label="Vocabulary URI"
+            />
+          </div>
+          <Field component={renderSectorTypeSelect} name={`${description}.vocabulary[code]`} label="Sector Code"/>
+          <div className="columns small-6">
+            <Field
+              name="SectorText"
+              type="text"
+              component={renderField}
+              label="Percentage"
+            />
+          </div>
         </div>
+        <div className="row">
+          <FieldArray
+            name={`${description}.additionalSector`}
+            component={renderNarrativeFields}
+            languageOptions={languageOptions}
+            textName="textSector"
+            textLabel="Title"
+          />
+        </div>
+      </div>
+    )}
+    <div className="columns">
+      <button className="control-button add" type="button" onClick={() => fields.push({})}>Add More</button>
+      <button
+        type="button"
+        title="Remove Title"
+        className="control-button remove float-right"
+        onClick={() => fields.pop()}>Delete
+      </button>
+      {touched && error && <span className="error">{error}</span>}
     </div>
+  </div>
 );
 class SectorForm extends React.Component {
 
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props)
+  }
 
-    render() {
-        const {activity} = this.props;
+  render() {
+    const {activity} = this.props;
 
-        return (
-            <div>
-                <div className="row">
-                    <div className="columns small-centered small-12">
-                        <h2 className="page-title with-tip">Sector</h2>
-                        <Tooltip className="inline" tooltip="Description text goes here">
-                            <i className="material-icons">info</i>
-                        </Tooltip>
-                        <div className="field-list">
-                            <div className="row">
-                                <Field component={renderSectorTypeSelect} name="regionPolicy[code]" label="Sector Vocabulary"/>
-                                <div className="columns small-6">
-                                    <Field
-                                        name="uriSectorText"
-                                        type="text"
-                                        component={renderField}
-                                        label="Vocabulary URI"
-                                    />
-                                </div>
-                            </div>
-                            <div className="row">
-                                <Field component={renderSectorTypeSelect} name="vocabularyPolicy[code]" label="Sector Code"/>
-                                <div className="columns small-6">
-                                    <Field
-                                        name="SectorText"
-                                        type="text"
-                                        component={renderField}
-                                        label="Percentage"
-                                    />
-                                </div>
-                            </div>
-                            <div className="row">
-                                <FieldArray
-                                    name="additionalTitles"
-                                    component={renderNarrativeFields}
-                                    languageOptions={activity["Language"]}
-                                    textName="textSectorTitle"
-                                    textLabel="Title"
-                                />
-                            </div>
-                        </div>
-                        <FieldArray name="additionalSector" component={renderSector} languageOptions={activity["Language"]}/>
-                    </div>
+    return (
+      <div>
+        <div className="row">
+          <div className="columns small-centered small-12">
+            <h2 className="page-title with-tip">Sector</h2>
+            <Tooltip className="inline" tooltip="Description text goes here">
+              <i className="material-icons">info</i>
+            </Tooltip>
+            <div className="field-list">
+              <div className="row">
+                <Field component={renderSectorTypeSelect} name="regionPolicy[code]" label="Sector Vocabulary"/>
+                <div className="columns small-6">
+                  <Field
+                    name="uriSectorText"
+                    type="text"
+                    component={renderField}
+                    label="Vocabulary URI"
+                  />
                 </div>
+              </div>
+              <div className="row">
+                <Field component={renderSectorTypeSelect} name="vocabularyPolicy[code]" label="Sector Code"/>
+                <div className="columns small-6">
+                  <Field
+                    name="SectorText"
+                    type="text"
+                    component={renderField}
+                    label="Percentage"
+                  />
+                </div>
+              </div>
+              <div className="row">
+                <FieldArray
+                  name="additionalTitles"
+                  component={renderNarrativeFields}
+                  languageOptions={activity["Language"]}
+                  textName="textSectorTitle"
+                  textLabel="Title"
+                />
+              </div>
             </div>
-        )
-    }
+            <FieldArray name="additionalSector" component={renderSector} languageOptions={activity["Language"]}/>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default reduxForm({
-    form: 'SectorForm',     // a unique identifier for this form
+  form: 'SectorForm',     // a unique identifier for this form
 })(SectorForm)
--- app/client/app/components/activity/forms/ClassificationsForm.js
+++ app/client/app/components/activity/forms/ClassificationsForm.js
@@ -6,43 +6,43 @@ import PolicyMakerForm from './ClassificationPolicyForm'
 
 class ClassificationForm extends React.Component {
 
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props)
+  }
 
-    render() {
-        const {handleSubmit, pristine, reset, submitting, previousPage, activity} = this.props;
+  render() {
+    const {handleSubmit, pristine, reset, submitting, previousPage, activity} = this.props;
 
-        return (
-            <div>
-                <div className="row controls">
-                    <div className="columns small-centered small-12">
-                        <h2 className="page-title with-tip">Classification</h2>
-                        <hr />
-                    </div>
-                </div>
-                <form onSubmit={handleSubmit}>
-                    <div className="row">
-                        <SectorForm activity={activity} />
-                    </div>
-                    <div className="row">
-                        <PolicyMakerForm activity={activity} />
-                    </div>
-                    <div className="row">
-                        <div className="columns small-12">
-                            <button type="button" className="button" onClick={previousPage}>Back to Geopolitical Form</button>
-                            <button className="button float-right" type="submit" disabled={submitting} onClick={handleSubmit}>
-                                Continue to Classification Form
-                            </button>
-                        </div>
-                    </div>
-                </form>
+    return (
+      <div>
+        <div className="row controls">
+          <div className="columns small-centered small-12">
+            <h2 className="page-title with-tip">Classification</h2>
+            <hr />
+          </div>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <SectorForm activity={activity}/>
+          </div>
+          <div className="row">
+            <PolicyMakerForm activity={activity}/>
+          </div>
+          <div className="row">
+            <div className="columns small-12">
+              <button type="button" className="button" onClick={previousPage}>Back to Geopolitical Form</button>
+              <button className="button float-right" type="submit" disabled={submitting} onClick={handleSubmit}>
+                Continue to Classification Form
+              </button>
             </div>
-        )
-    }
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 export default reduxForm({
-    form: 'ClassificationForm',
-    destroyOnUnmount: false,
+  form: 'ClassificationForm',
+  destroyOnUnmount: false,
 
 })(ClassificationForm)
